d34f78ec71cb92e3769f3ac0dc4dd9bffb62d5e4
Oliwia <dsw54518@student.dsw.edu.pl>
2024-05-07 17:45:33 +0200

    Lab03

---
+++ lab03.sh
@@ -0,0 +1,132 @@
+git commit
+git commit
+git branch bugFix
+git checkout bugFix
+git branch bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+git branch bugFix
+git checkout bugFix
+git commit
+git checckout main
+git commit
+git checkout bugFix
+git rebase main
+git checkout C4
+git checkout C4^
+git branch -f bugFix C0~0
+git branch -f main C6~0
+git checkout C1
+git reset HEAD~1
+git checkout pushed
+git revert HEAD
+git cherry-pick C3 C4 C7
+git rebase -i HEAD~4
+git checkout main
+git cherry-pick C4
+git rebase -i HEAD~2
+git commit --amend
+git rebase -i HEAD~2
+git branch -f main HEAD~0
+git checkout main
+git cherry-pick C2
+git checkout C1
+git cherry-pick C2 C3
+git branch -f main HEAD~0
+git tag v0 C1
+git tag V1 C2
+git checkout C2
+git describe main
+git describe side
+git commit
+git checkout bugFix
+git rebase main
+git checkout C4
+git rebase bugFix
+git checkout C5
+git rebase C4'
+git checkout C6
+git checkout C5'
+git checkout C7
+git rebase C6'
+git branch -f main HEAD~0
+git checkout HEAD~^2~1
+git branch bugWork
+git checkout main
+git rebase -i HEAD~4
+git branch -f one HEAD~0
+git checkout C2
+git branch -f three HEAD~0
+git checkout C5
+git rebase -i HEAD~4
+git branch -f two HEAD~0
+git checkout C5
+git branch -f main HEAD~0
+git clone
+git commit
+git checkout o/main
+git commit
+git fetch
+git pull
+git clone
+git fakeTeamwork main 2
+git commit
+git checkout C1
+git fetch
+git checkout C3
+git merge C4
+git branch -f main HAED~0
+git checkout main
+git commit
+git commit
+git push
+git clone
+git fakeTeamwork main 1
+git commit
+git fetch
+git rebase o/main
+git push
+git branch feature
+git push origin feature
+git barnch -f main HEAD~1
+git checkout feature
+git fetch
+git checkout side1
+git rebase o/main
+git checkout side2
+git rebase side1
+git checkout side3
+git rebase side2
+git branch -f main HEAD~0
+git checkout main
+git push
+git fetch
+git checkout side1
+git merge o/main
+git merge side2
+git merge side3
+git branch -f main HEAD~0
+git checkout main
+git push
+git checkout -b side o/main
+git commit
+git fetch
+git rebase o/main
+git push
+git push origin main
+git push origin foo
+git push origin main^:foo
+git push origin foo:main
+git fetch origin C3:foo
+git checkout C1
+git fetch origin C6:main
+git checkout foo
+git merge main
+git fetch origin :bar
+git push origin :foo
+git pull origin C3:foo
+git pull origin C2:side
+
