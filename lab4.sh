f0ce0b61d35efae4ee80fa7c73c53f2ef8b4441b
Oliwia <dsw54518@student.dsw.edu.pl>
2024-05-14 18:58:13 +0200

    three

--- lab4.sh
+++ lab4.sh
@@ -10,5 +10,19 @@ if [[ $1 == "--date" ]]; then
 	date
 fi
 
+function logs {
+	for ((i=1; i<=100; i++)); do
+		echo "Nazwa pliku: log$i.txt" > log$i.txt
+		echo "Nazwa skryptu: lab4.sh" >> log$i.txt
+		echo "data: $(date)" >> log$i.txt
+	done
+}
+
+if [[ $1 == "--logs" ]]; then
+	if [[ -z $2 ]]; then
+		logs
+	fi
+fi
+
 
 
