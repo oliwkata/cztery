5637cb580349bffce926b7551a9da367cabc97ce
Oliwia <dsw54518@student.dsw.edu.pl>
2024-05-14 18:19:16 +0200

    dodanie dwa

--- LAB04/lab4.sh
+++ LAB04/lab4.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 if [[ $# -eq 0 ]]; then
+<<<<<<< HEAD
 	echo "NIe podałeś żadnych argumentów."
 elif [[ $1 == "--date" ]]; then
 	date
+=======
+	echo "Nie podałeś argumentów."
+elif [[ $1 == "--date" ]]; then
+	date
+fi
+>>>>>>> jeden
+
+echo "log*.txt" > .gitignore
+
 
